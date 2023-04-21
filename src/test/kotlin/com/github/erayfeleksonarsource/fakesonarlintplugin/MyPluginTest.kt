20b67b33923ae69d31bc18c360e8efb7ff67e63d
Eray Felek <eray.felek@sonarsource.com>
2023-04-21 17:21:18 +0200

    test count

--- src/test/kotlin/com/github/erayfeleksonarsource/fakesonarlintplugin/MyPluginTest.kt
+++ src/test/kotlin/com/github/erayfeleksonarsource/fakesonarlintplugin/MyPluginTest.kt
@@ -1,5 +1,7 @@
 package com.github.erayfeleksonarsource.fakesonarlintplugin
 
+import com.github.erayfeleksonarsource.fakesonarlintplugin.listeners.HIGHLIGHT_TOPIC
+import com.github.erayfeleksonarsource.fakesonarlintplugin.listeners.HighlightListener
 import com.github.erayfeleksonarsource.fakesonarlintplugin.services.HighlightExternalAnnotator
 import com.intellij.ide.highlighter.XmlFileType
 import com.intellij.openapi.components.service
@@ -8,18 +10,28 @@ import com.intellij.testFramework.TestDataPath
 import com.intellij.testFramework.fixtures.BasePlatformTestCase
 import com.intellij.util.PsiErrorElementUtil
 import com.github.erayfeleksonarsource.fakesonarlintplugin.services.MyProjectService
+import com.intellij.util.messages.MessageBusConnection
+import com.intellij.util.messages.Topic
+import org.junit.Assert
 
 @TestDataPath("\$CONTENT_ROOT/src/test/testData")
-class MyPluginTest : BasePlatformTestCase() {
+class MyPluginTest : BasePlatformTestCase(), HighlightListener {
     fun testMessageBus() {
         val connection = project.messageBus.connect()
-        //connection.subscribe(HighlightExternalAnnotator.HIGLIGHT_TOPIC, this)
+        connection.subscribe(HIGHLIGHT_TOPIC, this)
+
+        myFixture.configureByFile("Asonar.java")
+        myFixture.checkHighlighting()
     }
 
     fun testHighlighting() {
         myFixture.configureByFile("Asonar.java")
 
         myFixture.checkHighlighting()
+    }
+
+    override fun notifyCount(count: Int) {
+        Assert.assertEquals(3, count)
     }
 
     override fun getTestDataPath() = "src/test/testData/MyPluginTest"
--- src/test/testData/MyPluginTest/Asonar.java
+++ src/test/testData/MyPluginTest/Asonar.java
@@ -1,5 +1,5 @@
 public class A<weak_warning descr="Unresolved property">sonar</weak_warning> {
-    void count<weak_warning>Eray</weak_warning>(){
+    void count<weak_warning>Eray</weak_warning>count<weak_warning>Eray</weak_warning>(){
         if("<weak_warning descr="Unresolved property">sonar</weak_warning><weak_warning>eray</weak_warning>" == null);
     }
 
