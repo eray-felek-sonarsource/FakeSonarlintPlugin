a5ae02fe3ca89136ab4981d21d5496d1c42fe0c7
Eray Felek <eray.felek@sonarsource.com>
2023-05-10 18:19:06 +0200

    merge tests

--- src/test/kotlin/com/github/erayfeleksonarsource/fakesonarlintplugin/TestUi.kt
+++ src/test/kotlin/com/github/erayfeleksonarsource/fakesonarlintplugin/TestUi.kt
@@ -5,7 +5,7 @@ import com.intellij.remoterobot.fixtures.ComponentFixture
 import com.intellij.remoterobot.search.locators.Locator
 import com.intellij.remoterobot.search.locators.byXpath
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Test
+import org.junit.Test
 
 
 class TestUi {
@@ -18,9 +18,9 @@ class TestUi {
 
         var fileMenu = remoteRobot.find(ComponentFixture::class.java, fileMenuLocator)
 
-        assertThat(fileMenu.hasText("File"))
-
         fileMenu.click()
+
+        assertThat(fileMenu.hasText("File"))
     }
 
 }
